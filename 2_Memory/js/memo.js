afb47c23365cb933cf025ccb01c8c1afc25a2608
SAICHITOO <84066132+SAICHITOO@users.noreply.github.com>
2021-12-01 15:17:47 +0900

    Add files via upload

--- 2_Memory/js/memo.js
+++ 2_Memory/js/memo.js
@@ -32,7 +32,7 @@ function saveLocalStorage() {
             window.alert("Key,Memoはいずれも必須です。");
             return;
         }else{
-        let w_confirm =confirm( "LocalStorage" + key + " " + value + "に保存しますか。.\nよろしいでしょうか");
+        let w_confirm =confirm( "LocalStorageに\n"  +"『"+ key + " " + value + "』"+"\n保存しますか。\nよろしいでしょうか");
         if(w_confirm ===true){
                localStorage.setItem(key,value);
                viewStorage() ;     //localStorageからのデータ取得とテーブル表示
@@ -54,17 +54,17 @@ function  delLocalStorage() {
   function(e) {
      e.preventDefault();
      let w_sel = "0"; //選択されていれば"1"件が返却される
-     w_sel = selectRadioBtn();
+     w_sel = selectCheckBox();
 
      if(w_sel === "1"){
       const key = document.getElementById("textKey").value;
       const value = document.getElementById("textMemo").value; 
-      let w_confirm =confirm( "LocalStorageから" + key + " " + value + "削除します。.\nよろしいでしょうか");
+      let w_confirm =confirm( "LocalStorageから\n"  +"『"+ key + " " + value + "』"+"\n削除します。.\nよろしいでしょうか");
       if(w_confirm ===true){
 
         localStorage.removeItem(key);
         viewStorage(); //LocalStorageからのデータのしゅとくとテーブルへ表示
-        let w_msg = "LocalStorageから" + key + " " + value + "を削除しました。";
+        let w_msg = "LocalStorageから"+"" + key + " " + value + "を削除しました。";
         window.alert(w_msg);
         document.getElementById("textKey").value = "";
         document.getElementById("textMemo").value = "";
@@ -99,50 +99,59 @@ function selectTable () {
   select.addEventListener("click",
      function(e) {
         e.preventDefault();
-        selectRadioBtn(); //テーブルからデータ選択
+        selectCheckBox();
     },false
   )
 };
 
 //テーブルからデータ選択
-function selectRadioBtn() {
+function selectCheckBox() {
    let w_sel ="0"; //選択されていれば "1"にする
-    const radio1 = document.getElementsByName("radio1");
-    const table1 = document.getElementById("table1");
-
-for(let i=0; i<radio1.length; i++){
-  if (radio1[i].checked){
-   document.getElementById("textKey").value=table1.rows[i+1].cells[1].firstChild.data;
-   document.getElementById("textMemo").value=table1.rows[i+1].cells[2].firstChild.data;
-   return w_sel = "1";
-  }
-}
-
-window.alert("1つ選択してください")
+   let w_cnt=0;
+     const chkbox1 = document.getElementsByName("chkbox1");
+     const table1 = document.getElementById("table1");
+        let w_textKey="";
+        let w_textMemo="";
+      for(let i=0; i<chkbox1.length; i++){
+        if (chkbox1[i].checked){
+          if(w_cnt===0){
+            w_textKey=table1.rows[i+1].cells[1].firstChild.data;
+            w_textMemo=table1.rows[i+1].cells[2].firstChild.data;
+          }
+          w_cnt++;
+        }
+      }
+      document.getElementById("textKey").value=w_textKey;
+      document.getElementById("textMemo").value=w_textMemo;
+      if(w_cnt===1){
+        return w_sel="1";
+      }else{
+         window.alert("1つ選択してください")
+      }
 };
-  //localStorageからのデータ取得とテーブル表示
-  function viewStorage(){
+//localStorageからのデータ取得とテーブル表示
+function viewStorage(){
     const list = document.getElementById("list");
     // htmlのテーブル初期化
     while(list.rows[0] ) list.deleteRow(0);
     //viewStorage すべての情報の取得
     for (let i=0; i<localStorage.length; i++){
-    let w_key =localStorage.key(i)
+        let w_key =localStorage.key(i)
 
-    //localStorageのキーと値を表示
-    let tr = document.createElement("tr");
-    let td1 = document.createElement("td");
-    let td2 = document.createElement("td");
-    let td3 = document.createElement("td");
-    list.appendChild (tr);
-    tr.appendChild (td1);
-    tr.appendChild (td2);
-    tr.appendChild (td3);
+        //localStorageのキーと値を表示
+        let tr = document.createElement("tr");
+        let td1 = document.createElement("td");
+        let td2 = document.createElement("td");
+        let td3 = document.createElement("td");
+        list.appendChild (tr);
+        tr.appendChild (td1);
+        tr.appendChild (td2);
+        tr.appendChild (td3);
 
-    td1.innerHTML = "<input name='radio1' type= 'radio'>";
-    td2.innerHTML = w_key;
-    td3.innerHTML = localStorage.getItem(w_key);
-  }
+        td1.innerHTML = "<input name='chkbox1' type= 'checkbox'>";
+        td2.innerHTML = w_key;
+        td3.innerHTML = localStorage.getItem(w_key);
+    }
   //sorter
   $("#table1").tablesorter({
       sortList:[[1,0]]
